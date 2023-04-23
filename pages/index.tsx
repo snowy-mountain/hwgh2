3f70d5f6deecf9ffe1d0173a620cbe1f69602a13
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-23 16:20:16 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,5 @@ export default function IndexPage() {
   window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
 </script>
     <script defer src="/_vercel/insights/script.js"></script>
-    </button>
   )
 }
