f9f13b5e2c1284ad59e8eb20720980907a00c8ba
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 16:41:20 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-echo "cat /vercel/path0/node_modules/next/dist/compiled/web-vitals/web-vitals.js"
-
-cat /vercel/path0/node_modules/next/dist/compiled/web-vitals/web-vitals.js
-
-echo "cat /vercel/path0/node_modules/next/dist/compiled/web-vitals/package.json"
-
-cat /vercel/path0/node_modules/next/dist/compiled/web-vitals/package.json
-
-echo "cat /vercel/path0/node_modules/next/dist/client/router"
-
-cat /vercel/path0/node_modules/next/dist/client/router
-
-echo "ls -la /vercel/path0/node_modules/next/dist/client/router"
-
-ls -la /vercel/path0/node_modules/next/dist/client/router
+which vercel
