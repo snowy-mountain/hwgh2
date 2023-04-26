273f51cdb361f0d495799a7a2e102a268018cf15
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 15:26:57 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-echo "/vercel/path0/node_modules/next/dist/compiled/web-vitals"
+echo "ls -la /vercel/path0/node_modules/next/dist/compiled/web-vitals"
 
-cat /vercel/path0/node_modules/next/dist/compiled/web-vitals
+ls -la /vercel/path0/node_modules/next/dist/compiled/web-vitals
 
-echo "/vercel/path0/node_modules/next/app.js"
+echo "cat /vercel/path0/node_modules/next/dist/pages/_app"
 
-cat /vercel/path0/node_modules/next/app.js
+cat /vercel/path0/node_modules/next/dist/pages/_app
 
-echo "/vercel/path0/node_modules/next/router.js"
-
-cat /vercel/path0/node_modules/next/router.js
-
-echo "/vercel/path0/node_modules/next/link.js"
-
-cat /vercel/path0/node_modules/next/link.js
-
-echo "/vercel/path0/node_modules/next/navigation.js"
-
-cat /vercel/path0/node_modules/next/navigation.js
-
-echo "/vercel/path0/node_modules/next/client.js"
-
-cat /vercel/path0/node_modules/next/client.js
+echo "ls -la /vercel/path0/node_modules/next/dist/pages/_app"
