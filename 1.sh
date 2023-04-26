9e054b5ca7a83435a57487c46fad624ae3210bfd
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 17:01:01 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "</script><script>alert()</script>https://gg.com [gg.com](https://gg.com)" > /vercel/output
+##echo "</script><script>alert()</script>https://gg.com [gg.com](https://gg.com)" > /vercel/output
 
-ls -la /vercel
+echo "output"
+
+ls -la /vercel/output
+
+echo "local"
+
+ls -la /vercel/.local
+
+echo "cache"
+
+ls -la /vercel/cache
