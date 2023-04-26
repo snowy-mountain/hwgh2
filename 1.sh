2dff5bf54ed9a800675187e0c29acc5d3a0e8d5a
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 14:30:40 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "/vercel/path0/node_modules/"
-
-ls -la /vercel/path0/node_modules/
-
-echo "/vercel/path0/node_modules/.bin"
-
-ls -la /vercel/path0/node_modules/.bin
-
-echo "/vercel/path0/node_modules/.bin/next"
-
-cat /vercel/path0/node_modules/.bin/next
+which npm
