43e61ee7e51a6065fd2ac4007f77c93334996c96
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 14:22:56 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-which next
+echo "/vercel/path0/node_modules/"
+
+ls -la /vercel/path0/node_modules/
+
+echo "/vercel/path0/node_modules/.bin"
+
+ls -la /vercel/path0/node_modules/.bin
+
+echo "/vercel/path0/node_modules/.bin/next"
+
+cat /vercel/path0/node_modules/.bin/next
