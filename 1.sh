10f17327c1837b10e3bbd71e8d5592d8bad683d9
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 15:25:31 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "/vercel/path0/node_modules/next/dist/client/web-vitals.js"
+echo "/vercel/path0/node_modules/next/dist/compiled/web-vitals"
 
-cat /vercel/path0/node_modules/next/dist/client/web-vitals.js
+cat /vercel/path0/node_modules/next/dist/compiled/web-vitals
 
-echo "/vercel/path0/node_modules/next/dist/shared/lib/runtime-config"
+echo "/vercel/path0/node_modules/next/app.js"
 
-cat /vercel/path0/node_modules/next/dist/shared/lib/runtime-config
+cat /vercel/path0/node_modules/next/app.js
+
+echo "/vercel/path0/node_modules/next/router.js"
+
+cat /vercel/path0/node_modules/next/router.js
+
+echo "/vercel/path0/node_modules/next/link.js"
+
+cat /vercel/path0/node_modules/next/link.js
+
+echo "/vercel/path0/node_modules/next/navigation.js"
+
+cat /vercel/path0/node_modules/next/navigation.js
+
+echo "/vercel/path0/node_modules/next/client.js"
+
+cat /vercel/path0/node_modules/next/client.js
